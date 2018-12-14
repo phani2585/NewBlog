21d4ee597c57bb18a5b81822ae006eb52d752112
phani2585 <44507567+phani2585@users.noreply.github.com>
2018-12-14 20:42:52 +0530

    Add files via upload

---
+++ src/main/java/Blog/MyFirstBlog.java
@@ -0,0 +1,11 @@
+package Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstBlog {
+    public static void main(String[] args){
+        SpringApplication.run(MyFirstBlog.class,args);
+    }
+}
